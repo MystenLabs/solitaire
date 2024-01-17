f6577dd864cf89be76284910249018a12325a75c
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-01-17 18:30:49 +0200

    SE-105 Create DifficultySelection component WIP

--- app/src/components/cards/Card.tsx
+++ app/src/components/cards/Card.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { cardIdToSvg } from "@/helpers/card_mappings";
 import Image from "next/image";
 
+
 export const Card = ({ id }: { id: number }) => {
     return (
         <div className={`card`}>
-           <Image src={cardIdToSvg(id)} alt={"Card"}/>
+           <Image src={cardIdToSvg(id)}
+                  alt={"Card"}/>
         </div>
     )
 }
--- app/src/components/difficultySelection/DifficultySelection.tsx
+++ app/src/components/difficultySelection/DifficultySelection.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import './styles.css'
+import { Card } from "../cards/Card";
+import {EasyMode} from "@/components/difficultySelection/difficultyModes/easyMode";
 
 export const DifficultySelection = () => {
     return (
         <div className={"outside-container"}>
-
-            <div className={"title"}>
+            <p className={"title"}>
                 Choose Your Difficulty
+            </p>
+            <div className={"modes-container"}>
+                <EasyMode />
+                <EasyMode />
             </div>
-
-
         </div>
     )
 }
