20494c290f97a55f1e84ab7e7125f630e3c689c2
Andreas Xydas <andreas@mystenlabs.com>
2024-01-18 17:03:44 +0200

    add gray card background and change description

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,9 @@
 import "server-only";
-import { Paper } from "@/components/general/Paper";
 import { LoginForm } from "@/components/forms/LoginForm";
 import { Metadata } from "next";
 import Image from "next/image";
 import logo from "../../public/SymbolRed 1.svg";
+import cardGray from "../../public/card-gray.svg";
 
 export const metadata: Metadata = {
   title: "PoC Template",
@@ -15,7 +15,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen space-y-20">
-      <Paper className="flex flex-col justify-center gap-10 items-center max-w-[600px] mx-auto">
+      <div className="flex flex-col justify-center gap-10 items-center max-w-[600px] mx-auto md:p-14 min-w-[200px] shadow-xl rounded-xl"
+        style={{
+          backgroundImage: `url(${cardGray.src})`,
+          backgroundSize: "cover", 
+          backgroundPosition: "center", 
+          backgroundRepeat: "no-repeat",
+        }}>
         <div className="flex space-x-1">
           <Image src={logo} alt="Logo" />
           <h2 className="text-xl text-MystenRed font-extrabold">Mysten Labs</h2>
@@ -24,13 +30,12 @@ export default function Home() {
           Sign In to Play Mysten Solitaire
         </h3>
         <p className="max-w-[300px] text-xs text-center font-normal">
-          Dive into the world of strategic moves and captivating gameplay in
-          this Klondike style Solitaire game. Whether you&apos;re here to beat your
-          high score or just starting, your next game awaits. Log in and let the
-          cards fall where they may!
+          Dive into the web3 version of Klondike style Solitaire game! 
+          This game is on-chain, meaning that you can interact with it and view your high score directly on the Sui network!
+          Log in and let the cards fall where they may!
         </p>
         <LoginForm />
-      </Paper>
+      </div>
       <div className="flex flex-col justify-center items-center">
         <p className="text-white text-xs font-normal">
           Learn more about MystenLabs at
