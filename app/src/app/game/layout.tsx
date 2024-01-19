1dd376615888149a4bd57a0a70d649449445d94c
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-01-19 15:02:41 +0200

    SE-145 Add banner and email to game layout (WIP)
    TODO
    - Difficulty selection gets out of board borders when
    shrinking the window size.
    - Missing Google image next to email.

--- app/src/app/game/layout.tsx
+++ app/src/app/game/layout.tsx
@@ -7,13 +7,25 @@ import { Spinner } from "@/components/general/Spinner";
 export default function MemberRootLayout({ children }: ChildrenProps) {
   const { user, isLoading } = useAuthentication();
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  if (user?.role === "anonymous") {
-    return "Not allowed";
-  }
-
-  return children;
+  return (
+      <div className="h-screen">
+          <div className={"flex align-bottom pt-10 px-20 justify-between"}>
+              <div className="logo text-white text-[28px] font-bold font-['Mysten Walter Alte']">
+                  Mysten Solitaire
+              </div>
+              <div className="email pl-2 pr-3.5 py-3 left-0 top-0 rounded-[36px] border border-white border-opacity-40 items-center max-h-12">
+                  <div className="text-center text-white text-base font-normal font-['Mysten Walter Alte'] leading-tight">
+                        {user?.email}
+                  </div>
+              </div>
+          </div>
+          {isLoading ? (
+              <Spinner/>
+          ) : user?.role === "anonymous" ? (
+              "Not allowed"
+          ) : (
+              children
+          )}
+      </div>
+  );
 }
