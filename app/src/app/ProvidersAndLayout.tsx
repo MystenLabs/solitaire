aec36c31fd4ea4261e03ced6fa892925685a933b
Andreas Xydas <andreas@mystenlabs.com>
2024-02-28 12:27:02 +0200

    Co-authored-by: Alexandros Tzimas <tzalex@outlook.com.gr> cleanup - 1st iteration

--- app/src/app/ProvidersAndLayout.tsx
+++ app/src/app/ProvidersAndLayout.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-// import { LargeScreenLayout } from "@/components/layouts/LargeScreenLayout";
-// import { MobileLayout } from "@/components/layouts/MobileLayout";
 import { AuthenticationProvider } from "@/contexts/Authentication";
-import { useIsMobile } from "@/hooks/useIsMobile";
 import { useRegisterServiceWorker } from "@/hooks/useRegisterServiceWorker";
 import { ChildrenProps } from "@/types/ChildrenProps";
 import React from "react";
@@ -12,7 +9,6 @@ import table from "../../public/Table.svg";
 
 export const ProvidersAndLayout = ({ children }: ChildrenProps) => {
   const _ = useRegisterServiceWorker();
-  const { isMobile } = useIsMobile();
 
   return (
     <AuthenticationProvider>
@@ -25,8 +21,6 @@ export const ProvidersAndLayout = ({ children }: ChildrenProps) => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        {/* {!!isMobile && <MobileLayout>{children}</MobileLayout>}
-        {!isMobile && <LargeScreenLayout>{children}</LargeScreenLayout>} */}
         {children}
         <Toaster
           position="bottom-center"
