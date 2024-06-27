e6c57d80c631a751735f811032c4a7aa26b09d1e
Sang Kim <nonvis@gmail.com>
2024-06-27 17:37:52 +0900

    Add a banner to display a disclaimer and a button to display game rules (#40)

--- app/src/app/ProvidersAndLayout.tsx
+++ app/src/app/ProvidersAndLayout.tsx
@@ -6,6 +6,7 @@ import { ChildrenProps } from "@/types/ChildrenProps";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import table from "../../public/Table.svg";
+import { InfoIcon } from "./InfoIcon";
 
 export const ProvidersAndLayout = ({ children }: ChildrenProps) => {
   const _ = useRegisterServiceWorker();
@@ -16,12 +17,16 @@ export const ProvidersAndLayout = ({ children }: ChildrenProps) => {
         className={`min-h-screen w-screen`}
         style={{
           backgroundImage: `url(${table.src})`, // Set the table image as background
-          backgroundSize: "cover", 
-          backgroundPosition: "bottom", 
+          backgroundSize: "cover",
+          backgroundPosition: "bottom",
           backgroundRepeat: "no-repeat",
         }}
       >
+        <div className="absolute top-0 w-full flex justify-evenly items-center bg-white py-3 px-5">
+          <span className="text-opacity-90 text-[14px] text-[#4F4F4F]">[Mysten Solitaire] is provided for testnet purposes only and does not involve real money or the opportunity to win real money.</span>
+        </div>
         {children}
+        <InfoIcon/>
         <Toaster
           position="bottom-center"
           toastOptions={{
