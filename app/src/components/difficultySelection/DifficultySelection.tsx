2b6186b333b5dbf74ae26eef333977011ae25203
Guilherme Macedo <guilherme.macedo@mystenlabs.com>
2025-09-08 18:25:49 +0300

    chore: balance check/testnet faucet removed

--- app/src/components/difficultySelection/DifficultySelection.tsx
+++ app/src/components/difficultySelection/DifficultySelection.tsx
@@ -4,65 +4,16 @@
 import { ModeVisual } from "@/components/difficultySelection/difficultyModes/modeVisual";
 import easy_mode_visual from "../../../public/assets/difficultyModesVisuals/easy_mode_visual.svg";
 import normal_mode_visual from "../../../public/assets/difficultyModesVisuals/normal_mode_visual.svg";
-import { useCurrentAccount } from "@mysten/dapp-kit";
-import { useEffect, useState } from "react";
-import BigNumber from "bignumber.js";
-import { getBalance } from "@/helpers/getBalance";
-import { SuiClient } from "@mysten/sui/client";
-import toast from "react-hot-toast";
-import { Spinner } from "../general/Spinner";
 
 export const DifficultySelection = ({
   onGameCreation,
 }: {
   onGameCreation: (mode: "easy" | "normal") => void;
 }) => {
-  const account = useCurrentAccount();
-  const [isLoading, setIsLoading] = useState(false);
-  const [enoughBalance, setEnoughBalance] = useState<boolean>(true);
-  const hasEnoughBalance = (balance: BigNumber) => {
-    return balance.isGreaterThanOrEqualTo(
-      new BigNumber(process.env.MINIMUM_BALANCE_TO_PLAY ?? 1500000000)
-    );
+  const startGame = (mode: "easy" | "normal") => {
+    onGameCreation(mode);
   };
-  useEffect(() => {
-    const fetchBalance = async () => {
-      const balance = await getBalance(account?.address!);
-      setEnoughBalance(hasEnoughBalance(balance));
-    };
 
-    fetchBalance();
-  }, [account?.address]);
-
-  const handleClickTopUp = async () => {
-    document.body.style.cursor = "wait";
-    setIsLoading(true);
-    const response = await fetch("https://pocs-faucet.vercel.app/api/faucet", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Enoki-api-key": process.env.NEXT_PUBLIC_ENOKI_API_KEY!,
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem("user")!)["zkLoginSession"]["jwt"]
-        }`,
-      },
-    });
-    const body = await response.json();
-    if (response.ok) {
-      const client = new SuiClient({
-        url: process.env.NEXT_PUBLIC_SUI_NETWORK!,
-      });
-      await client.waitForTransaction({
-        digest: body.txDigest,
-      });
-      toast.success("Top up successful!");
-      setEnoughBalance(hasEnoughBalance(await getBalance(account?.address!)));
-    } else {
-      toast.error(`Top up failed: ${body.error}`);
-    }
-    document.body.style.cursor = "default";
-    setIsLoading(false);
-  };
   return (
     <div className="flex flex-col items-center justify-center rounded-3xl bg-white px-10 backdrop-blur-2xl">
       <p
@@ -74,11 +25,9 @@ export const DifficultySelection = ({
       </p>
       <div className={"modes-container grid grid-cols-2 gap-5 pb-14"}>
         <button
-          onClick={async () => {
-            onGameCreation("easy");
+          onClick={ () => {
+            startGame("easy");
           }}
-          // TODO: instead of disabling we should show a toast message
-          // disabled={!enoughBalance}
         >
           <ModeVisual
             level={"Easy"}
@@ -87,11 +36,9 @@ export const DifficultySelection = ({
           />
         </button>
         <button
-          onClick={async () => {
-            onGameCreation("normal");
+          onClick={ () => {
+            startGame("normal");
           }}
-          // TODO: instead of disabling we should show a toast message
-          // disabled={!enoughBalance}
         >
           <ModeVisual
             level={"Normal"}
@@ -100,22 +47,6 @@ export const DifficultySelection = ({
           />
         </button>
       </div>
-      {!enoughBalance && (
-        <div className="flex flex-col justify-center">
-          <p className="mb-1 font-light text-gray-500">
-            Looks like you do not have enough balance to play.
-          </p>
-          <button
-            className={`mb-10 rounded-md ${
-              isLoading ? "bg-gray-300" : "bg-black"
-            } p-2 text-white`}
-            onClick={handleClickTopUp}
-          >
-            {isLoading && <Spinner />}
-            {!isLoading && <>💧Click here to topup!</>}
-          </button>
-        </div>
-      )}
     </div>
   );
 };
--- app/src/components/user/accountDropdown.tsx
+++ app/src/components/user/accountDropdown.tsx
@@ -4,58 +4,68 @@ import Image from "next/image";
 import google from "../../../public/assets/logos/google_email.svg";
 import React from "react";
 import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuItem,
-    DropdownMenuContent,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuContent,
 } from "@radix-ui/react-dropdown-menu";
 import { CaretDownIcon } from "@radix-ui/react-icons";
-import {useCurrentAccount, useDisconnectWallet} from "@mysten/dapp-kit";
+
+import {
+  useCurrentAccount,
+  useDisconnectWallet,
+} from "@mysten/dapp-kit";
 
 import toast from "react-hot-toast";
-import { getSession } from "@mysten/enoki";
+import { formatString } from "@/helpers/formatString";
 
 export const AccountDropdown = () => {
-    const currentAccount = useCurrentAccount();
-    // TODO: Fix call of the session and remove comments
-    // const session = getSession(currentAccount as WalletAccount);
-    const {mutate: disconnect} = useDisconnectWallet();
-    const logout = async () => {
-        disconnect();
-        localStorage.clear();
-        window.location.replace("/");
-    };
+  const currentAccount = useCurrentAccount();
+  const address = currentAccount?.address;
+  const { mutate: disconnect } = useDisconnectWallet();
 
-    const copyAddress = () => {
-        navigator.clipboard.writeText(currentAccount?.address || "");
-        toast.success("Address copied to clipboard");
-    }
+  const copyAddress = () => {
+    if (!address) return;
+    navigator.clipboard.writeText(address);
+    toast.success("Address copied to clipboard");
+  };
 
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <button>
-                    <div
-                        className="select-none flex gap-2 email pl-2 pr-3.5 py-3 left-0 top-0 rounded-[36px] border border-white border-opacity-40 items-center max-h-12 min-w-max">
-                        <div className={"shrink-0"}>
-                            <Image src={google} alt={"Logo of google"}/>
-                        </div>
-                        <div
-                            className="text-center text-white text-base font-normal font-['Mysten Walter Alte'] leading-tight">
-                            {/* {currentAccount?.email} */}
-                        </div>
-                        <CaretDownIcon color={"white"}/>
-                    </div>
-                </button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem onSelect={logout} className={"select-none rounded-[36px] border border-white px-14 py-1 text-center text-white bg-white bg-opacity-10 text-base leading-tight cursor-pointer"}>
-                    👋 Logout
-                </DropdownMenuItem>
-                <DropdownMenuItem onSelect={copyAddress} className={"select-none rounded-[36px] border border-white px-14 py-1 text-center text-white bg-white bg-opacity-10 text-base leading-tight cursor-pointer"}>
-                    {/* {user?.address.slice(0, 10)+'...'} */}
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
-}
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <button>
+          <div className="select-none flex gap-2 email pl-2 pr-3.5 py-3 left-0 top-0 rounded-[36px] border border-white border-opacity-40 items-center max-h-12 min-w-max">
+            <div className={"shrink-0"}>
+              <Image src={google} alt={"Logo of google"} />
+            </div>
+            <div className="text-center text-white text-base font-normal font-['Mysten Walter Alte'] leading-tight">
+                {
+                    // TODO: Check if the email can be added
+                }
+              {formatString(address ?? "", 10)}
+            </div>
+            <CaretDownIcon color={"white"} />
+          </div>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          onSelect={() => disconnect()}
+          className={
+            "select-none rounded-[36px] border border-white px-14 py-1 text-center text-white bg-white bg-opacity-10 text-base leading-tight cursor-pointer"
+          }
+        >
+          👋 Logout
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onSelect={copyAddress}
+          className={
+            "select-none rounded-[36px] border border-white px-14 py-1 text-center text-white bg-white bg-opacity-10 text-base leading-tight cursor-pointer"
+          }
+        >
+          {formatString(address ?? "", 10)}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
