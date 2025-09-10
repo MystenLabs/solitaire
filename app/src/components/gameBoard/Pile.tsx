512197e1c02830d86bad2eeceaf8eb5d636bcf94
Guilherme Macedo <guilherme.macedo@mystenlabs.com>
2025-09-10 12:07:59 +0300

    chore: update on Pile import

--- app/src/components/gameBoard/Pile.tsx
+++ app/src/components/gameBoard/Pile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import {Card} from "../cards/Card";
-import {Pile as PileProps} from "../../models/pile";
+import {Card} from "@/components/cards/Card";
+import {Pile as PileProps} from "@/models/pile";
 
 
 export default function Pile({pile}: { pile: PileProps }) {
